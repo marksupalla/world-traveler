dcff217b7c64f304e798ddf882fb0cae752826f2
Mark Supalla <marksupalla@gmail.com>
2014-08-18 15:53:21 -0500

    added map and add vacation

--- app/static/js/user/home.js
+++ app/static/js/user/home.js
@@ -1,8 +1,18 @@
+/* global google:true */
+
 (function(){
   'use strict';
 
+var map;
+
   $(document).ready(function(){
+    initMap(36.2, -86.7, 9);   
   });
 
+  function initMap(lat, lng, zoom){
+    var styles     = [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}],    
+        mapOptions = {center: new google.maps.LatLng(lat, lng), zoom: zoom, mapTypeId: google.maps.MapTypeId.ROADMAP, styles: styles};
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  }
 })();
 
